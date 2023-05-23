9c77d35ef5f8076742e7b71dd2f885490c75cf2b
Shanley Caswell <shanleycaswell@gmail.com>
2023-05-23 13:44:39 -0400

    first class functions

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function receivesAFunction(callback){
+    let list = ["BS" , "More BS" , "Even More BS!"]
+    callback(list)
+}
+
+function functionOfNothing(bs){       
+    
+}
+receivesAFunction(loopThroughArray)
+
+function add(num1, num2){
+    return add(num1 + num2)
+}
+
+
+function returnsANamedFunction(something){
+    return add
+}
+function returnsAnAnonymousFunction(blank) {
+    return function(){}
+}
